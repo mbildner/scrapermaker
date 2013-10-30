d27cbbcf563e21ba30d8841961670781a27eb198
Marcus Ljungblad <marcus@ljungblad.nu>
2013-10-30 18:48:23 -0400

    initial structure to parse annotated html

---
+++ parser.py
@@ -0,0 +1,27 @@
+import sys
+from bs4 import BeautifulSoup
+
+class Parser:
+
+  def submit(self, html):
+    soup = BeautifulSoup(html)
+    # grab all the elements that we added
+    # find out what type of data makes most sense to extract 
+    # apply different strategies depending on attributes surrounding, 
+    # i.e., from the first row in a tables, get the column (how is this represented?)
+    grabthis = [ tag for tag in soup.find_all() if tag.has_attr('data-grabthis')]
+    print grabthis
+
+
+def main(filename):
+  parser = Parser()
+  with open(filename, 'r') as html:
+    parser.submit(html.read())
+
+
+if __name__ == '__main__':
+  if len(sys.argv) > 1:
+    print 'Parsing ' + sys.argv[1]
+    main(sys.argv[1])
+  else: 
+    print 'You might want some sample files to go with that...'
