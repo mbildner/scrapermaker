d5c76ee169ad78dfa7f6ec1a0b62b8287e3f848c
Marcus Ljungblad <marcus@ljungblad.nu>
2013-10-30 19:27:03 -0400

    parser extracts tag's position in DOM

--- parser.py
+++ parser.py
@@ -3,13 +3,28 @@ from bs4 import BeautifulSoup
 
 class Parser:
 
+  def clean(self, tag): 
+    title = tag.get('data-grabthis')
+    content = tag.text.lstrip('***GRAB THIS: {{%s}}*** ' % title)
+    tagtype = tag.name
+    position = self.position(tag)
+    return (title, content, tagtype, position)
+
+  def position(self, tag, pos=[]):
+    if not tag.parent:
+      pos.reverse()
+      return pos
+    else:
+      pos.append((tag.name, tag.attrs))
+      return self.position(tag.parent, pos)
+
   def submit(self, html):
     soup = BeautifulSoup(html)
     # grab all the elements that we added
     # find out what type of data makes most sense to extract 
     # apply different strategies depending on attributes surrounding, 
     # i.e., from the first row in a tables, get the column (how is this represented?)
-    grabthis = [ tag for tag in soup.find_all() if tag.has_attr('data-grabthis')]
+    grabthis = [ self.clean(tag) for tag in soup.find_all() if tag.has_attr('data-grabthis')]
     print grabthis
 
 
